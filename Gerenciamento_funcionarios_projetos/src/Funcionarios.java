d1bb158ea4ab4ba15237f8b4b3e8c2d8b0f52dc5
vinicius <viniamenic@gmail.com>
2024-09-26 00:33:06 -0300

    organizando repositório para começar a desenvolver o projeto

---
+++ Gerenciamento_funcionarios_projetos/src/Funcionarios.java
@@ -0,0 +1,6 @@
+public class Funcionarios {
+    // A classe Funcionario deve ter atributos como nome, cargo e salário. . Implemente dois construtores para cada classe, um com todos os atributos e outro com um número reduzido de parâmetros.
+
+
+    //atributos da classe
+}
---
+++ Gerenciamento_funcionarios_projetos/src/Projetos.java
@@ -0,0 +1,7 @@
+public class Projetos {
+    // A classe Projeto deve ter nome, prazo e um objeto Funcionario responsável. . Implemente dois construtores para cada classe, um com todos os atributos e outro com um número reduzido de parâmetros.
+
+    //atributos da classe
+
+
+}
