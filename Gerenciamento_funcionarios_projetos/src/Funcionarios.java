087513675f3d8a581e6eef6386cf9168bbfd9ab7
vinicius <viniamenic@gmail.com>
2024-09-26 21:07:36 -0300

    continue..

--- Gerenciamento_funcionarios_projetos/src/Funcionarios.java
+++ Gerenciamento_funcionarios_projetos/src/Funcionarios.java
@@ -1,6 +1,8 @@
 public class Funcionarios {
     // A classe Funcionario deve ter atributos como nome, cargo e salário. . Implemente dois construtores para cada classe, um com todos os atributos e outro com um número reduzido de parâmetros.
 
-
     //atributos da classe
+
+
+    //metodos da classe
 }
