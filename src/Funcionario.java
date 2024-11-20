52fb3751177d18063925b540a14b7e6035666e2f
Louie Nery <louie.nery1500@gmail.com>
2024-11-20 14:39:47 -0300

    Update Funcionario.java

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,67 +1,27 @@
-import java.util.ArrayList;
-
-public class Funcionario {
+class Funcionario {
     private String nome;
     private String cargo;
     private double salario;
-    /*
-     * Utilizei este metodo para poder armazenar todos os funcionarios criados dentro de uma Array, quando for necessario fazer pesquisa de funcionario faça-a pelo 
-     * atributo "Nome".
-     */
-    private static ArrayList<Funcionario> funcionarios = new ArrayList<>();
 
+    // Construtor completo
     public Funcionario(String nome, String cargo, double salario) {
         this.nome = nome;
         this.cargo = cargo;
         this.salario = salario;
     }
 
+    // Construtor com salário padrão
     public Funcionario(String nome, String cargo) {
-        this.nome = nome;
-        this.cargo = cargo;
-        this.salario = 0;
+        this(nome, cargo, 0.0);
     }
 
-    public void adicionarFuncionario(String nome, String cargo, float salario) {
-        Funcionario f = new Funcionario(nome, cargo, salario);
-        funcionarios.add(f);
-        System.out.println("Funcionário " + nome + " adicionado.");
-    }
+    // Getters e Setters
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-    public void removerFuncionario(String nome) {
-        for (int i = 0; i < funcionarios.size(); i++) {
-            if (funcionarios.get(i).nome.equalsIgnoreCase(nome)) {
-                funcionarios.remove(i);
-                System.out.println("Funcionário " + nome + " foi removido.");
-                return; 
-            }
-        }
-        System.out.println("Funcionário " + nome + " não foi encontrado.");
-    }
-    
-    public String getNome() {
-        return nome;
-    }
-    
-    public String getCargo() {
-        return cargo;
-    }
+    public String getCargo() { return cargo; }
+    public void setCargo(String cargo) { this.cargo = cargo; }
 
-    public String getInfo() {
-        return "Nome: " + nome + ", Cargo: " + cargo + ", Salário: " + salario;
-    }
-
-    public String getInfoSemSalario() {
-        return "Nome: " + nome + ", Cargo: " + cargo;
-    }
-
-    public void exibirInfo() {
-        System.out.println(getInfo());
-    }
-
-    public void listarFuncionarios() {
-        for (Funcionario f : funcionarios) {
-            f.exibirInfo();
-        }
-    }
+    public double getSalario() { return salario; }
+    public void setSalario(double salario) { this.salario = salario; }
 }
