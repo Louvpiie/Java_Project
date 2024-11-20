d5c8a19065776edeaf5893b7af7a2e21b156acd9
Louie Nery <louie.nery1500@gmail.com>
2024-11-20 14:42:59 -0300

    Update Projeto.java

--- src/Projeto.java
+++ src/Projeto.java
@@ -1,59 +1,33 @@
 import java.time.LocalDate;
 
-public class Projeto {
+class Projeto {
     private String nome;
     private LocalDate prazo;
     private Funcionario responsavel;
 
-    // Construtor com todos os atributos
+    // Construtor completo
     public Projeto(String nome, LocalDate prazo, Funcionario responsavel) {
         this.nome = nome;
         this.prazo = prazo;
         this.responsavel = responsavel;
     }
 
-    // Construtor com parâmetros reduzidos
+    // Construtor sem responsável
     public Projeto(String nome, LocalDate prazo) {
         this(nome, prazo, null);
     }
 
     // Getters e Setters
-    public String getNome() {
-        return nome;
-    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
+    public LocalDate getPrazo() { return prazo; }
+    public void setPrazo(LocalDate prazo) { this.prazo = prazo; }
 
-    public LocalDate getPrazo() {
-        return prazo;
-    }
+    public Funcionario getResponsavel() { return responsavel; }
+    public void setResponsavel(Funcionario responsavel) { this.responsavel = responsavel; }
+    public void removeResponsavel() { this.responsavel = null; }
 
-    public void setPrazo(LocalDate prazo) {
-        this.prazo = prazo;
-    }
-
-    public Funcionario getResponsavel() {
-        return responsavel;
-    }
-
-    public void setResponsavel(Funcionario responsavel) {
-        this.responsavel = responsavel;
-    }
-
-    // Método para adicionar um funcionário ao projeto
-    public void adicionarFuncionario(Funcionario funcionario) {
-        this.responsavel = funcionario;
-    }
-
-    // Método para remover o funcionário do projeto
-    public void removerFuncionario() {
-        this.responsavel = null;
-    }
-
-    // Método para validar se o prazo é maior que a data atual
-    public boolean validarPrazo() {
-        return this.prazo.isAfter(LocalDate.now());
-    }
+    // Valida se o prazo do projeto é no futuro
+    public boolean prazoValido() { return !this.prazo.isBefore(LocalDate.now()); }
 }
